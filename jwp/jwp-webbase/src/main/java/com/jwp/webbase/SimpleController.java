3b55c76e6569b3a6ac317852269e55359dd3d106
amon256 <amon256@126.com>
2015-12-29 18:37:08 +0800

    escape

--- jwp/jwp-webbase/src/main/java/com/jwp/webbase/SimpleController.java
+++ jwp/jwp-webbase/src/main/java/com/jwp/webbase/SimpleController.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
